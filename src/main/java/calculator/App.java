5c38f77948f5b7b9780ad1688fb5131e92245922
Robbie <robbie331@>
2024-07-30 17:42:41 +0900

    1단계 완료

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -1,9 +1,16 @@
 package calculator;
 
+import java.util.Scanner;
+
 public class App {
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-        System.out.println("test");
+        System.out.print("첫 번째 숫자를 입력하세요 : ");
+        int num1 = sc.nextInt();
+        System.out.print("두 번째 숫자를 입력하세요 : ");
+        int num2 = sc.nextInt();
+
     }
 }
